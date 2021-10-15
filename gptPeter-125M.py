58b5bd560867bc650fa1e66795a20bb9a7e8059a
peter szemraj <peter.szemraj@gmail.com>
2021-10-15 04:08:58 +0200

    print out transcript at end

--- gptPeter-125M.py
+++ gptPeter-125M.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     print("loaded model - ", datetime.now())
 
     stay_in_chat = True
-
+    p_list = []
     while stay_in_chat:
         user_query = str(input("enter your prompt here (write 'exit' to exit) -->")).lower()
         if user_query == 'exit':
@@ -27,31 +27,41 @@ if __name__ == "__main__":
         st = time.time()
         p_list = []
         p_list.append(user_query + "\n")
+        p_list.append("\n")
         p_list.append("peter szemraj:" + "\n")
         this_prompt = "".join(p_list)
         print("\n... generating... \n")
         this_result = ai.generate(
             n=1,
-            top_k=10,
-            batch_size=10,
+            top_k=20,
+            batch_size=20,
             max_length=64,
             min_length=16,
             prompt=this_prompt,
-            temperature=0.7, top_p=0.9, do_sample=True, return_as_list=True,
+            temperature=0.75,
+            top_p=0.9, do_sample=True, return_as_list=True,
         )
 
-        this_result = str(this_result[0]).split('\n')
-        if verbose: print("the type of the result is {} and length is {}".format(type(this_result),
-                                                                                 len(this_result)))
+        try:
+            this_result = str(this_result[0]).split('\n')
+            if verbose: print("the type of the result is {} and length is {}".format(type(this_result),
+                                                                                     len(this_result)))
+            res_out = [str(ele).strip() for ele in this_result]
+            p_out = [str(ele).strip() for ele in p_list]
+            diff_list = list(set(res_out).difference(p_out))  # remove prior prompts for the response
+            this_result = [str(msg) for msg in diff_list if ":" not in str(msg)]  # remove all names
 
-        if len(this_result) > 2:
-            output = str(this_result[2]).strip()
-        elif isinstance(this_result, list):
-            output = str(" ".join(this_result)).strip()
-        else:
-            output = this_result
+            # TODO: if the first element in the list is short (say <10 chars) and a second element exists, join them
+
+            if not isinstance(this_result, list): list(this_result)
+            output = str(this_result[0]).strip()
+        except:
+            print("... model response is too short or something. try continuing the conv")
+            output = "what?"
 
         pp.pprint(output, indent=4)
+        p_list.append(output + "\n")
+        p_list.append("\n")
         # pp.pprint(this_result[3].strip(), indent=4)
         rt = round(time.time() - st, 1)
         gc.collect()
@@ -59,3 +69,5 @@ if __name__ == "__main__":
         print("took {runtime} seconds to generate. \n".format(runtime=rt))
 
     print("finished - ", datetime.now())
+    print("A transcript of your chat is as follows: \n")
+    pp.pprint(p_list)
--- gptPeter_gpt2_335M.py
+++ gptPeter_gpt2_335M.py
@@ -42,22 +42,21 @@ if __name__ == "__main__":
             top_p=0.9, do_sample=True, return_as_list=True,
         )
 
-        this_result = str(this_result[0]).split('\n')
-        if verbose: print("the type of the result is {} and length is {}".format(type(this_result),
-                                                                                 len(this_result)))
-        res_out = [str(ele).strip() for ele in this_result]
-        p_out = [str(ele).strip() for ele in p_list]
-        diff_list = list(set(res_out).difference(p_out))  # remove prior prompts for the response
-        this_result = [str(msg) for msg in diff_list if ":" not in str(msg)]  # remove all names
+        try:
+            this_result = str(this_result[0]).split('\n')
+            if verbose: print("the type of the result is {} and length is {}".format(type(this_result),
+                                                                                     len(this_result)))
+            res_out = [str(ele).strip() for ele in this_result]
+            p_out = [str(ele).strip() for ele in p_list]
+            diff_list = list(set(res_out).difference(p_out))  # remove prior prompts for the response
+            this_result = [str(msg) for msg in diff_list if ":" not in str(msg)]  # remove all names
+            # TODO: if the first element in the list is short (say <10 chars) and a second element exists, join them
 
-        pp.pprint(this_result, indent=8, compact=True)
-        output = str(this_result[0]).strip()
-        # if len(this_result) > 2:
-        #     output = str(this_result[2]).strip()
-        # elif isinstance(this_result, list):
-        #     output = str(" ".join(this_result)).strip()
-        # else:
-        #     output = this_result
+            if not isinstance(this_result, list): list(this_result)
+            output = str(this_result[0]).strip()
+        except:
+            print("... model response is too short or something. try continuing the conv")
+            output = "what?"
 
         pp.pprint(output, indent=4)
         p_list.append(output + "\n")
@@ -69,3 +68,5 @@ if __name__ == "__main__":
         print("took {runtime} seconds to generate. \n".format(runtime=rt))
 
     print("finished - ", datetime.now())
+    print("A transcript of your chat is as follows: \n")
+    pp.pprint(p_list)
