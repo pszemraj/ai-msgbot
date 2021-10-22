2a5e226c4a1cc33aabfc8199b755325f02ff2ae4
peter szemraj <peter.szemraj@gmail.com>
2021-10-22 02:36:41 +0200

    :sparkles: add compatability + model download for 75ksteps DailyDialogue 355M model (pretrained) ✨

--- download_models.py
+++ download_models.py
@@ -7,7 +7,7 @@ model_links = {
     "gpt-neo-125M_150ks_checkpoint": "https://www.dropbox.com/sh/e2hbxkzu1e4vtte/AACdUHz-J735F5Cn-KV4udlka?dl=1",
     "gpt2_std_gpu_774M_60ksteps":"https://www.dropbox.com/sh/2wu7nbckqo5ghga/AABMg6SUaaP103WcL2lnF2b7a?dl=1",
     "gpt2_std_gpu_774M_120ksteps":"https://www.dropbox.com/sh/f8pocv18n0bohng/AACVMXcWR9Kn_CQsZKqpF1xoa?dl=1",
-
+    "gpt2_dailydialogue_355M_75Ksteps":"https://www.dropbox.com/sh/ahx3teywshods41/AACrGhc_Qntw6GuX7ww-3pbBa?dl=1",
 }
 if __name__ == "__main__":
 
@@ -24,10 +24,13 @@ if __name__ == "__main__":
             model_links["gpt-neo-125M_150ks_checkpoint"], extract_loc=os.getcwd()
         )
 
-    # if "gpt2_std_gpu_774M_60ksteps" not in folder_names:
-    #     utils.get_zip_URL(
-    #         model_links["gpt2_std_gpu_774M_60ksteps"], extract_loc=os.getcwd()
-    #     )
+    if "gpt2_dailydialogue_355M_75Ksteps" not in folder_names:
+        # this is the generic" chatbot pretrain, that other models should be trained from. it can also be used standalone.. but untest
+        utils.get_zip_URL(
+            model_links["ggpt2_dailydialogue_355M_75Ksteps"], extract_loc=os.getcwd()
+        )
+
+
 
     if "gpt2_std_gpu_774M_120ksteps" not in folder_names:
         # current PROD model in telegram
